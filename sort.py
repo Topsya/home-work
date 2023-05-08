8b9ee66dde30276c5d65271a60ab0997171c0aed
Topsya <topsya1986@gmail.com>
2023-05-08 18:50:15 +0300

    18.50    08.05.23

--- sort.py
+++ sort.py
@@ -1,10 +1,10 @@
-import Path 
-import pathlib
+ # import Path 
+from pathlib import Path
+ 
 import shutil
 import os
-import re
-import sys
 
+import re
 
 
 #path = r'c:\user\Desktop\Мотлох'
@@ -69,29 +69,29 @@ def move_files(path):
          old_path = path + file
          new_path = path + "/archives/" + file
          shutil.move(old_path,new_path)
-      return razresh
+   return razresh
 
-list_vse_razresh = list(set(razresh)) # залишаю лише унікальні розширення
-list_music =  os.listdir(path + "/audio/") 
-list_video = os.listdir('/user/Desktop/Мотлох/video/') 
-list_foto = os.listdir('/user/Desktop/Мотлох/images/') 
-list_doc  = os.listdir('/user/Desktop/Мотлох/documents/') 
-list_archiv =os.listdir('/user/Desktop/Мотлох/archives/') 
-list_neizvestnie = os.listdir('/user/Desktop/Мотлох') 
+# list_vse_razresh = set(razresh) # залишаю лише унікальні розширення
+# list_music =  os.listdir( path + "/audio/") 
+# list_video = os.listdir('/user/Desktop/Мотлох/video/') 
+# list_foto = os.listdir('/user/Desktop/Мотлох/images/') 
+# list_doc  = os.listdir('/user/Desktop/Мотлох/documents/') 
+# list_archiv =os.listdir('/user/Desktop/Мотлох/archives/') 
+# list_neizvestnie = os.listdir('/user/Desktop/Мотлох')
 
-print ({ 
-              'list_music':  list_music,
-              'list_video': list_video,
-              'list_foto': list_foto,
-              'list_doc': list_doc,
-              'list_archiv': list_archiv,
-              'list_vse_razresh': list_vse_razresh,
-              'list_neizvestnie': list_neizvestnie,
-            })
+# print ({ 
+#               'list_music':  list_music,
+#               'list_video': list_video,
+#               'list_foto': list_foto,
+#               'list_doc': list_doc,
+#               'list_archiv': list_archiv,
+#               'list_vse_razresh': list_vse_razresh,
+#               'list_neizvestnie': list_neizvestnie,
+#             })
      
 # Список файлів в кожній категорії (музика, відео, фото та ін.)
 # Перелік усіх відомих скрипту розширень, які зустрічаються в цільовій папці.
 # Перелік всіх розширень, які скрипту невідомі.
 
 path = Path(input('Введіть путь папки : '))
-def move_files (sys.argv[1])
+move_files (Path(path)) 
