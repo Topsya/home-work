a4e40c2809750d2fa1ac691fadef6e4bff6c6373
Topsya <topsya1986@gmail.com>
2023-05-09 15:43:02 +0300

    1111111

--- sort.py
+++ sort.py
@@ -18,14 +18,17 @@ TRANS = {}
 for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
     TRANS[ord(c)] = l
 
-
+path = Path(input('Введіть путь папки : '))
 def normalize(file):
 #  приймає на вхід рядок та повертає рядок;
- new_name = ""
+ # new_name = ""
+ name = file.split(".")[0]
+ exis = file.split(".")[1]
     # здійснює транслітерацію кириличних символів на латиницю;
  new_name  = file.translate(TRANS)               
   # замінює всі символи, крім літер латинського алфавіту та цифр, на символ '_'; 
- new_name  = re.sub(r'\W+','_',new_name)
+ ispravl = re.sub(r'W+','_',name)
+ new_name  = f'{ispravl}.{exis}'
 
  return  new_name 
 # транслітерація може не відповідати стандарту, але бути читабельною;
@@ -33,65 +36,76 @@ def normalize(file):
 
 
 def move_files(path):
+ #  print(os.listdir(path))
  # змінюємо назву
    for file in os.listdir(path):
-       if file.is_file(): 
-        file = os.rename(f'{file }', f'{normalize(file)} ')
+        if os.path.isfile(file): 
+          file = os.rename(os.path.join(path, file), os.path.join(path,normalize(file)))
     
-   razresh = []
+   # razresh = []
    imeges = ['jpeg', 'png', 'jpg', 'svg']
    documents = ['doc', 'docx', 'txt', 'pdf', 'xlsx', 'pptx']
    audio = ['mp3', 'ogg', 'wav', 'amr']
    video = ['avi', 'mp4', 'mov', 'mkv']
    archives = ['zip', 'gz', 'tar']
+   Spisok_papok = ('imeges','documents','audio','video','archives')
+   
+   # створюєм папки
+   for folder in Spisok_papok:
+      if not os.path.exists(f'{path}\\{folder}'):
+         os.mkdir(f'{path}\\{folder}')
+
    Spisok = os.listdir(path)  
    for file in Spisok :
-      extension = file.split(".")
-      razresh += extension[1]   #збираю всі розширення
+      #  if os.path.isfile(file):
+         extension = file.split(".")
+            #збираю всі розширення
+     # razresh += extension[1]   
        
-      if  len(extension) > 1 and extension[1].lower() in imeges :
-         old_path = path + file
-         new_path = path + "/images/" + file
-         shutil.move(old_path,new_path)  
-      elif  len(extension) > 1 and extension[1].lower() in documents :
-         old_path = path + file
-         new_path = path + "/documents/" + file
-         shutil.move(old_path,new_path)
-      elif  len(extension) > 1 and extension[1].lower() in audio :
-         old_path = path + file
-         new_path = path + "/audio/" + file
-         shutil.move(old_path,new_path)  
-      elif  len(extension) > 1 and extension[1].lower() in video :
-         old_path = path + file
-         new_path = path + "/video/" + file
-         shutil.move(old_path,new_path)  
-      elif  len(extension) > 1 and extension[1].lower() in archives :
-         old_path = path + file
-         new_path = path + "/archives/" + file
-         shutil.move(old_path,new_path)
-   return razresh
+         if  len(extension) > 1 and extension[1].lower() in imeges :
+            old_path = os.path.join(path, file)
+            new_path = os.path.join(path,'imeges',file)
+            shutil.move(old_path,new_path)  
+         elif  len(extension) > 1 and extension[1].lower() in documents :
+            old_path = os.path.join(path, file)
+            new_path = os.path.join(path,'documents',file)
+            shutil.move(old_path,new_path)
+         elif  len(extension) > 1 and extension[1].lower() in audio :
+            old_path = os.path.join(path, file)
+            new_path = os.path.join(path,'audio',file)
+            shutil.move(old_path,new_path)  
+         elif  len(extension) > 1 and extension[1].lower() in video :
+            old_path = os.path.join(path, file)
+            new_path = os.path.join(path,'video',file)
+            shutil.move(old_path,new_path)  
+         elif  len(extension) > 1 and extension[1].lower() in archives :
+            old_path = os.path.join(path, file)
+            new_path = os.path.join(path,'archives',file)
+            shutil.move(old_path,new_path)
+    #  return razresh
 
-# list_vse_razresh = set(razresh) # залишаю лише унікальні розширення
-# list_music =  os.listdir( path + "/audio/") 
-# list_video = os.listdir('/user/Desktop/Мотлох/video/') 
-# list_foto = os.listdir('/user/Desktop/Мотлох/images/') 
-# list_doc  = os.listdir('/user/Desktop/Мотлох/documents/') 
-# list_archiv =os.listdir('/user/Desktop/Мотлох/archives/') 
-# list_neizvestnie = os.listdir('/user/Desktop/Мотлох')
+  # list_vse_razresh = set(razresh) 
+# # залишаю лише унікальні розширення
+   list_music =  os.listdir(os.path.join(path,'audio')) 
+   list_video = os.listdir(os.path.join(path,'video')) 
+   list_foto = os.listdir(os.path.join(path,'imeges'))
+   list_doc  = os.listdir(os.path.join(path,'documents'))
+   list_archiv =os.listdir(os.path.join(path,'archives'))
+   list_neizvestnie = path.glob('*.*')
 
-# print ({ 
-#               'list_music':  list_music,
-#               'list_video': list_video,
-#               'list_foto': list_foto,
-#               'list_doc': list_doc,
-#               'list_archiv': list_archiv,
-#               'list_vse_razresh': list_vse_razresh,
-#               'list_neizvestnie': list_neizvestnie,
-#             })
+   print ({  
+              'list_music':  list_music,
+              'list_video': list_video,
+              'list_foto': list_foto,
+              'list_doc': list_doc,
+              'list_archiv': list_archiv,
+     #         'list_vse_razresh': list_vse_razresh,
+              'list_neizvestnie': list_neizvestnie,
+            })
      
 # Список файлів в кожній категорії (музика, відео, фото та ін.)
 # Перелік усіх відомих скрипту розширень, які зустрічаються в цільовій папці.
 # Перелік всіх розширень, які скрипту невідомі.
 
-path = Path(input('Введіть путь папки : '))
-move_files (Path(path)) 
+
+move_files (path)
