f22dc8acdd3291a6070dd15c43d99e93fd1b1555
Topsya <topsya1986@gmail.com>
2023-06-08 22:26:11 +0300

    1

---
+++ homeworkKLASS.py
@@ -0,0 +1,28 @@
+from collections import UserDict
+
+
+
+class Field:
+
+class Name:
+    Name = 
+class Phone:
+self
+
+class AddressBook(UserDict):
+     # додамо логіку пошуку за записами до цього класу
+     # Record = add_record
+     def __init__(self):
+        super().__init__() 
+    # return value in self.data.values()    
+class Record():
+# додавання/видалення/редагування необов'язкових полів
+#  та зберігання обов'язкового поля Name
+
+
+
+
+
+# Клас Name, обов'язкове поле з ім'ям.
+# Клас Phone, необов'язкове поле з телефоном та таких один 
+# запис (Record) може містити кілька.
