52a39a9961a7bb4f88c49640b9b6ec34a9b040d5
Topsya <topsya1986@gmail.com>
2023-06-23 14:31:35 +0300

    23.06

--- homework12.py
+++ homework12.py
@@ -9,14 +9,26 @@ def json_file(AdresBook):
        print('AdresBook was creeate in json.')
 
 def unpacked_file():
-    with open('users_fail.json') as readfail:
+    with open('users_fail.json', 'r', encoding='utf-8') as readfail:
        unpacked_users = json.load(readfail) 
        print("AdresBook was unpacked.")
        return unpacked_users
 
-
+def find_name_phone():
+   unpacked_users = unpacked_file()
+   vod_poisk = input('enter Name or Phone of abonent : ')
+   # unpacked_users = unpacked_users.encod('utf-8')
+   for i in unpacked_users:
+        for value in i.values():
+            if vod_poisk in value:
+                print (f'  {i} ')
+            else: 
+                print ('no matches found in AdresBook')
 
 
 if __name__ == '__main__':
-    create_book(fake,AdresBook, n=4)
-    json_file(AdresBook)
+#    create_book(fake,AdresBook, n=4)
+#    json_file(AdresBook)
+   unpacked_file()
+   find_name_phone()
+   
