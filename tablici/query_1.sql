ff8f1c521faa04ef3ae61c987ce7cd151065ea61
Topsya <topsya1986@gmail.com>
2023-09-12 22:06:42 +0300

    1

--- tablici/query_1.sql
+++ tablici/query_1.sql
@@ -2,7 +2,10 @@
 
 -- Знайти 5 студентів із найбільшим середнім балом з усіх предметів.
 
-SELECT    student_id  ,  grade , name 
+SELECT  st.name , sub.name , avg(grade )
 FROM grades as g
-JOIN students as st on st.id  = g.student_id 
-limit 5 
+JOIN students as st on st.id  = g.student_id  
+JOIN subjects as sub on sub.id  = g.subject_id  
+--WHERE  
+ORDER BY avg(grade) 
+limit 5
