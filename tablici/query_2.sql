b6d1a757d0566a9284c92da3abc1bb58409a281e
Topsya <topsya1986@gmail.com>
2023-09-12 23:07:24 +0300

    1

--- tablici/query_2.sql
+++ tablici/query_2.sql
@@ -1,8 +1,9 @@
 
 -- Знайти студента із найвищим середнім балом з певного предмета.
 
-SELECT      st.name , sub.name , g.grade 
+SELECT  st.name , sub.name ,AVG(grade)
 FROM grades as g
 JOIN students as st on st.id  = g.student_id  
 JOIN subjects as sub on sub.id  = g.subject_id  
-limit 8
+WHERE  subject_id = 2
+ORDER BY avg(grade)
