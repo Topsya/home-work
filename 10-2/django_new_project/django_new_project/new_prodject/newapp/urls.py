011f91816cf05eb70c3f65078ff629f3a7bdbe22
Topsya <topsya1986@gmail.com>
2023-10-07 09:54:29 +0300

    07.10

--- 10-2/django_new_project/django_new_project/new_prodject/newapp/urls.py
+++ 10-2/django_new_project/django_new_project/new_prodject/newapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('author_add/', views.author_add, name='author_add'),
     path('quotes_add/', views.quotes_add, name='quotes_add'),
-    path("<int:page>", views.quotes, name="quotes"),
-    path('author/<str:author_url>/', views.author, name='author'),
+    path('author/', views.author, name='author'),
+    path('quotes/', views.quotes, name='quotes'),
 ]
 
--- 10-2/django_new_project/django_new_project/new_prodject/newapp/views.py
+++ 10-2/django_new_project/django_new_project/new_prodject/newapp/views.py
@@ -4,28 +4,47 @@ from django.shortcuts import render, redirect
 from .forms import AuthorsForm, QuotesForm
 from django.contrib.auth.decorators import login_required
 from .conect_to_mongodb import db
-from django.core.paginator import Paginator
+
 
 # Create your views here.
+quotes =  db.quotes 
 
 def main(request):
     return render(request, 'newapp/index.html')
 
-def quotes (request, page=1):
-#     db = uri.quotes_db
-    quotes = db.quotes.find()
-    # print (quotes)
-#     per_page = 10
-    # paginator = Paginator(list(quotes))#, per_page)
-#     quotes_on_page = paginator.page(page)
-    return render(request, 'quotes_app/index.html', context={'quotes': quotes })
+# def quotes2 (request, page=1):
+# #     db = uri.quotes_db
+#     quotes2 = db.quotes.find()
+#     # print (quotes)
+# #     per_page = 10
+#     # paginator = Paginator(list(quotes))#, per_page)
+# #     quotes_on_page = paginator.page(page)
+#     return render(request, 'newapp/index.html', context={'quotes': quotes })
 
 
 def author (request, author_url):
 #     db = uri.quotes_db
     author_name = author_url.replace('%20', ' ')
     author = db.author.find_one({'fullname': author_name})
-    return render(request, 'quotes_app/author.html', context={'author': author})
+    return render(request, 'newapp/author.html', context={'author': author})
+
+def quotes(request):
+    quotes = quotes.objects.all()
+
+    if request.method == 'POST':
+        form = QuotesForm(request.POST)
+        if form.is_valid():
+            new_quotes = form.save()
+
+            choice_quotes = quotes.objects.filter(name__in=request.POST.getlist('tags'))
+            for tag in choice_quotes.iterator():
+                new_quotes.quotes.add(tag)
+
+            return redirect(to='newapp:main')
+        else:
+            return render(request, 'newapp/quotes.html', {"quotes": quotes, 'form': form})
+
+    return render(request, 'newapp/quotes.html', {"quotes": quotes, 'form': QuotesForm()})
 
 @login_required
 def author_add(request):
