928d44c1cad0732d6c1d5db4d73e6444e179f74e
Topsya <topsya1986@gmail.com>
2023-10-29 18:21:54 +0200

    1212

---
+++ 14-2/jwt_na_fast_api/alembic/versions/59f91e73b80b_new.py
@@ -0,0 +1,26 @@
+"""new
+
+Revision ID: 59f91e73b80b
+Revises: 
+Create Date: 2023-10-29 17:46:36.559393
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '59f91e73b80b'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
