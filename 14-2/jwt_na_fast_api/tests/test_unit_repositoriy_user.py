571826b2b6ddb77578cf69b68c8d051db5cb43ee
Topsya <topsya1986@gmail.com>
2023-10-29 21:27:59 +0200

    29.10.23       time  21 : 30

--- 14-2/jwt_na_fast_api/tests/test_unit_repositoriy_user.py
+++ 14-2/jwt_na_fast_api/tests/test_unit_repositoriy_user.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock
 
 from sqlalchemy.orm import Session
 
-from jwt_na_fast_api.shemes import UserModel
+from jwt_na_fast_api.schemas import UserModel
 from jwt_na_fast_api.database.models import User
 from jwt_na_fast_api.repository.users import (get_user_by_email, create_user, delete_user, update_token,
                                           confirmed_email, update_avatar, change_password
@@ -23,68 +23,68 @@ class TestUsersRepository(unittest.IsolatedAsyncioTestCase):
         result = await get_user_by_email(email=email, db=self.session)
         self.assertEqual(result, user)
 
-    # async def test_get_user_by_email_nonexistent(self):
-    #     user_email = "non_existent_mail@example.com"
-    #     self.session.query().filter_by().first.return_value = None
+    async def test_get_user_by_email_nonexistent(self):
+        user_email = "non_existent_mail@example.com"
+        self.session.query().filter_by().first.return_value = None
 
-    #     result = await get_user_by_email(email=user_email, db=self.session)
-    #     self.assertIsNone(result)
+        result = await get_user_by_email(email=user_email, db=self.session)
+        self.assertIsNone(result)
 
-    # async def test_create_user(self):
-    #     user_data = UserModel(username="fake_user", user_email="test@example.com", password="password123")
-    #     new_user = User(id=1, user_email=user_data.user_email)
-    #     self.session.add.return_value = None
-    #     self.session.commit.return_value = None
-    #     self.session.refresh.return_value = None
+    async def test_create_user(self):
+        user_data = UserModel(username="fake_user", user_email="test@example.com", password="password123")
+        new_user = User(id=1, user_email=user_data.user_email)
+        self.session.add.return_value = None
+        self.session.commit.return_value = None
+        self.session.refresh.return_value = None
 
-    #     result = await create_user(body=user_data, db=self.session)
-    #     self.assertEqual(result.user_email, new_user.user_email)
+        result = await create_user(body=user_data, db=self.session)
+        self.assertEqual(result.user_email, new_user.user_email)
 
-    # async def test_delete_user(self):
-    #     user = User(id=1, user_email="test_mail@example.com")
-    #     self.session.delete.return_value = None
-    #     self.session.commit.return_value = None
-    #     self.session.refresh.return_value = None
+    async def test_delete_user(self):
+        user = User(id=1, user_email="test_mail@example.com")
+        self.session.delete.return_value = None
+        self.session.commit.return_value = None
+        self.session.refresh.return_value = None
 
-    #     await delete_user(user=user, db=self.session)
-    #     self.assertEqual(self.session.delete.call_args[0][0], user)
+        await delete_user(user=user, db=self.session)
+        self.assertEqual(self.session.delete.call_args[0][0], user)
 
-    # async def test_update_token(self):
-    #     user = User(id=1, user_email="test_mail@example.com")
-    #     token = "new_refresh_token"
-    #     self.session.commit.return_value = None
-    #     self.session.refresh.return_value = None
+    async def test_update_token(self):
+        user = User(id=1, user_email="test_mail@example.com")
+        token = "new_refresh_token"
+        self.session.commit.return_value = None
+        self.session.refresh.return_value = None
 
-    #     await update_token(user=user, token=token, db=self.session)
-    #     self.assertEqual(user.refresh_token, token)
+        await update_token(user=user, token=token, db=self.session)
+        self.assertEqual(user.refresh_token, token)
 
-    # async def test_confirmed_email(self):
-    #     user_email = "test_mail@example.com"
-    #     user = User(id=1, user_email=user_email, confirmed=True)
-    #     self.session.commit.return_value = None
-    #     self.session.refresh.return_value = None
+    async def test_confirmed_email(self):
+        user_email = "test_mail@example.com"
+        user = User(id=1, user_email=user_email, confirmed=True)
+        self.session.commit.return_value = None
+        self.session.refresh.return_value = None
 
-    #     await confirmed_email(email=user_email, db=self.session)
-    #     self.assertTrue(user.confirmed)
+        await confirmed_email(email=user_email, db=self.session)
+        self.assertTrue(user.confirmed)
 
-    # async def test_update_avatar(self):
-    #     user_email = "test_mail@example.com"
-    #     avatar_url = "https://fake.com/avatar.png"
-    #     self.session.commit.return_value = None
-    #     self.session.refresh.return_value = None
+    async def test_update_avatar(self):
+        user_email = "test_mail@example.com"
+        avatar_url = "https://fake.com/avatar.png"
+        self.session.commit.return_value = None
+        self.session.refresh.return_value = None
 
-    #     updated_user = await update_avatar(email=user_email, avatar_url=avatar_url, db=self.session)
-    #     self.assertEqual(updated_user.avatar_url, avatar_url)
+        updated_user = await update_avatar(email=user_email, avatar_url=avatar_url, db=self.session)
+        self.assertEqual(updated_user.avatar_url, avatar_url)
 
-    # async def test_change_password(self):
-    #     user_email = "test_mail@example.com"
-    #     new_password = "new_password123"
-    #     user = User(id=1, user_email=user_email, password="old_password123")
-    #     self.session.commit.return_value = None
-    #     self.session.refresh.return_value = None
+    async def test_change_password(self):
+        user_email = "test_mail@example.com"
+        new_password = "new_password123"
+        user = User(id=1, user_email=user_email, password="old_password123")
+        self.session.commit.return_value = None
+        self.session.refresh.return_value = None
 
-    #     updated_user = await change_password(user=user, password=new_password, db=self.session)
-    #     self.assertEqual(updated_user.password, new_password)
+        updated_user = await change_password(user=user, password=new_password, db=self.session)
+        self.assertEqual(updated_user.password, new_password)
 
 
 if __name__ == '__main__':
