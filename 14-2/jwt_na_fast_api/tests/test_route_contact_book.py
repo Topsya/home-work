3b46e4be999e537f5630e79f28ecd979f54121e1
Topsya <topsya1986@gmail.com>
2023-10-30 20:06:55 +0200

    12123213213

--- 14-2/jwt_na_fast_api/tests/test_route_contact_book.py
+++ 14-2/jwt_na_fast_api/tests/test_route_contact_book.py
@@ -7,9 +7,9 @@ from jwt_na_fast_api.services.auth import auth_service
 
 
 @pytest.fixture()
-def test_create_access_token(client, user, session, monkeypatch):
+def token(client, user, session, monkeypatch):
     mock_send_email = MagicMock()
-    monkeypatch.setattr("src.routes.auth.send_email", mock_send_email)
+    monkeypatch.setattr("jwt_na_fast_api.routes.auth.send_email", mock_send_email)
     client.post("/api/auth/signup", json=user)
     current_user: User = session.query(User).filter(User.email == user.get('email')).first()
     current_user.confirmed = True
@@ -22,106 +22,106 @@ def test_create_access_token(client, user, session, monkeypatch):
     return data["access_token"]
 
 
-# def test_create_tag(client, token):
-#     with patch.object(auth_service, 'r') as r_mock:
-#         r_mock.get.return_value = None
-#         response = client.post(
-#             "/api/tags",
-#             json={"name": "test_tag"},
-#             headers={"Authorization": f"Bearer {token}"}
-#         )
-#         assert response.status_code == 201, response.text
-#         data = response.json()
-#         assert data["name"] == "test_tag"
-#         assert "id" in data
+def test_create_contact(client, token):
+    with patch.object(auth_service, 'r') as r_mock:
+        r_mock.get.return_value = None
+        response = client.post(
+            "/api/contacts",
+            json={"name": "test_contact"},
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        assert response.status_code == 201, response.text
+        data = response.json()
+        assert data["name"] == "test_contact"
+        assert "id" in data
 
 
-# def test_get_tag(client, token):
-#     with patch.object(auth_service, 'r') as r_mock:
-#         r_mock.get.return_value = None
-#         response = client.get(
-#             "/api/tags/1",
-#             headers={"Authorization": f"Bearer {token}"}
-#         )
-#         assert response.status_code == 200, response.text
-#         data = response.json()
-#         assert data["name"] == "test_tag"
-#         assert "id" in data
+def test_read_contact(client, token):
+    with patch.object(auth_service, 'r') as r_mock:
+        r_mock.get.return_value = None
+        response = client.get(
+            "/api/contacts/1",
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert data["name"] == "test_contact"
+        assert "id" in data
 
 
-# def test_get_tag_not_found(client, token):
-#     with patch.object(auth_service, 'r') as r_mock:
-#         r_mock.get.return_value = None
-#         response = client.get(
-#             "/api/tags/2",
-#             headers={"Authorization": f"Bearer {token}"}
-#         )
-#         assert response.status_code == 404, response.text
-#         data = response.json()
-#         assert data["detail"] == "Tag not found"
+def test_read_contact_not_found(client, token):
+    with patch.object(auth_service, 'r') as r_mock:
+        r_mock.get.return_value = None
+        response = client.get(
+            "/api/contacts/2",
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        assert response.status_code == 404, response.text
+        data = response.json()
+        assert data["detail"] == "Contact not found"
 
 
-# def test_get_tags(client, token):
-#     with patch.object(auth_service, 'r') as r_mock:
-#         r_mock.get.return_value = None
-#         response = client.get(
-#             "/api/tags",
-#             headers={"Authorization": f"Bearer {token}"}
-#         )
-#         assert response.status_code == 200, response.text
-#         data = response.json()
-#         assert isinstance(data, list)
-#         assert data[0]["name"] == "test_tag"
-#         assert "id" in data[0]
+def test_read_contacts(client, token):
+    with patch.object(auth_service, 'r') as r_mock:
+        r_mock.get.return_value = None
+        response = client.get(
+            "/api/contacts",
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert isinstance(data, list)
+        assert data[0]["name"] == "test_contact"
+        assert "id" in data[0]
 
 
-# def test_update_tag(client, token):
-#     with patch.object(auth_service, 'r') as r_mock:
-#         r_mock.get.return_value = None
-#         response = client.put(
-#             "/api/tags/1",
-#             json={"name": "new_test_tag"},
-#             headers={"Authorization": f"Bearer {token}"}
-#         )
-#         assert response.status_code == 200, response.text
-#         data = response.json()
-#         assert data["name"] == "new_test_tag"
-#         assert "id" in data
+def test_update_contact(client, token):
+    with patch.object(auth_service, 'r') as r_mock:
+        r_mock.get.return_value = None
+        response = client.put(
+            "/api/contacts/1",
+            json={"name": "new_test_contact"},
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert data["name"] == "new_test_contact"
+        assert "id" in data
 
 
-# def test_update_tag_not_found(client, token):
-#     with patch.object(auth_service, 'r') as r_mock:
-#         r_mock.get.return_value = None
-#         response = client.put(
-#             "/api/tags/2",
-#             json={"name": "new_test_tag"},
-#             headers={"Authorization": f"Bearer {token}"}
-#         )
-#         assert response.status_code == 404, response.text
-#         data = response.json()
-#         assert data["detail"] == "Tag not found"
+def test_update_contact_not_found(client, token):
+    with patch.object(auth_service, 'r') as r_mock:
+        r_mock.get.return_value = None
+        response = client.put(
+            "/api/contacts/2",
+            json={"name": "new_test_contact"},
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        assert response.status_code == 404, response.text
+        data = response.json()
+        assert data["detail"] == "Contact not found"
 
 
-# def test_delete_tag(client, token):
-#     with patch.object(auth_service, 'r') as r_mock:
-#         r_mock.get.return_value = None
-#         response = client.delete(
-#             "/api/tags/1",
-#             headers={"Authorization": f"Bearer {token}"}
-#         )
-#         assert response.status_code == 200, response.text
-#         data = response.json()
-#         assert data["name"] == "new_test_tag"
-#         assert "id" in data
+def test_remove_contact(client, token):
+    with patch.object(auth_service, 'r') as r_mock:
+        r_mock.get.return_value = None
+        response = client.delete(
+            "/api/contacts/1",
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert data["name"] == "new_test_contact"
+        assert "id" in data
 
 
-# def test_repeat_delete_tag(client, token):
-#     with patch.object(auth_service, 'r') as r_mock:
-#         r_mock.get.return_value = None
-#         response = client.delete(
-#             "/api/tags/1",
-#             headers={"Authorization": f"Bearer {token}"}
-#         )
-#         assert response.status_code == 404, response.text
-#         data = response.json()
-#         assert data["detail"] == "Tag not found"
+def test_repeat_remove_contact(client, token):
+    with patch.object(auth_service, 'r') as r_mock:
+        r_mock.get.return_value = None
+        response = client.delete(
+            "/api/contacts/1",
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        assert response.status_code == 404, response.text
+        data = response.json()
+        assert data["detail"] == "Contact not found"
